261329dd4964e535a6466227b1ab9b2278e30204
Rachmad Aprisandy <rachmadblackspade212@gmail.com>
2024-12-15 14:50:59 +0700

    commit again

--- Percobaan2_18.java
+++ Percobaan2_18.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
-public class Percobaan2_18{
 
-    static int hitungPangkat(int x,int y){
-        if ( y == 0){
-            return(1);
-        }else{
-            return(x *hitungPangkat(x, y -1));
+public class Percobaan2_18 {
+    static int hitungPangkat(int x, int y) {
+        if (y == 0) {
+            System.out.print("1"); 
+            return 1;
+        } else {
+            System.out.print(x + (y > 1 ? "x" : "")); 
+            return x * hitungPangkat(x, y - 1);
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int bilangan,pangkat;
+        int bilangan, pangkat;
 
-        System.out.print("Bilangan Yang Dihitung : ");
+        System.out.print("Bilangan yang dihitung: ");
         bilangan = sc.nextInt();
-        System.out.print("pangkat : ");
+        System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        System.out.print("Perhitungan: ");
+        int hasil = hitungPangkat(bilangan, pangkat);
+        System.out.println(" = " + hasil); 
     }
 }
